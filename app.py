32c56730038e6e7ef39e8a8ddbc1624565753ad3
Boroche <chafik.smida@gmail.com>
2024-01-09 21:29:13 +0100

    add a function test

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
-
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/hello')
 def hello():
     return 'Hello World'
+
+def add(x):
+    return x+1
+
 
 app.run(host='localhost', port=5000)
